53ee93c66695260c307063706d98ffaebf1063b5
tasmankingsley <tasman.kingsley@gmail.com>
2022-09-11 14:54:41 +0900

    first commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+
+<style scoped>
+:global(body) {
+    height: 100%;
+    background-color: #dad9d4;
+    text-align: center;
+    font-size: 24px;
+    font-weight: 300;
+  } 
+</style>
+
---
+++ components/Nav.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="top-bar">
+    <div class="title">artist website</div>
+    <div class="nav">
+        <div class="link">
+            <NuxtLink to="/" active-class="active">Home</NuxtLink>
+        </div>
+
+        <div class="link">
+            <NuxtLink to="/gallery" active-class="active">Gallery</NuxtLink>
+        </div>
+
+        <div class="link">
+            <NuxtLink to="/blog" active-class="active">Blog</NuxtLink>
+        </div>
+        
+        <div class="link">
+            <NuxtLink to="/about" active-class="active">About</NuxtLink>
+        </div>
+
+    </div>
+</div>
+</template>
+
+<style scoped>
+.top-bar {
+    display: grid;
+    grid-template-rows: repeat(2, 1fr);
+    height: 100px;
+    width: 100%;
+    background-color: #fff;
+    
+}
+
+.title {
+    font-size: 1.8rem;
+    text-align: center;
+    place-self: center;
+    font-weight: 300;
+}
+
+.nav {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    /* gap: 10px; */
+    cursor: pointer;
+}
+.link {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    place-items: center;
+
+
+}
+
+.link:hover, .link:active {
+    
+}
+
+a, a:visited, a:hover, a:active {
+    font-size: 1.5rem;
+    font-weight: 300;
+    height: 40px;
+    line-height: 40px;
+    width: 80%;
+    border-radius: 5px;
+    text-align: center;
+
+    color: inherit;
+    /* text-decoration: none; */
+}
+
+.active, a:hover {
+    background-color: #dad9d4;
+    border-radius: 5px;
+}
+</style>
